add411932e600187416f86fab6bf30d944e6879f
Alex Kibler <alexkibler@me.com>
2017-12-11 19:29:40 -0500

    added functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
+# comment controller
 class CommentsController < ApplicationController
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @comment = @question.comments.new(comment_params)
+    @comment.score = 1
+    if @comment.save!
+      redirect_to question_comment_path(@question, @comment)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,27 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:question_id])
+    @question = Question.find(params[:id])
+    @comments = @question.comments
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.score = 1
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question)
   end
 end
