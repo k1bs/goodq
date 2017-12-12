eb4e421c3a7e1915e7f24ead009af7df68a9f2f5
Alex Kibler <alexkibler@me.com>
2017-12-12 16:26:00 -0500

    update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'questions#index'
   resources :questions, except: [:edit] do
     resources :comments, only: [:new, :create, :destroy] do
       post '/upvote', to: 'comments#upvote', as: 'upvote'
